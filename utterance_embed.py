5cb87f496575b983dbfdbc479b71771048452daf
Jude Lee <dongyub63@gmail.com>
2017-08-19 16:20:58 +0900

    Edit word embedding

--- utterance_embed.py
+++ utterance_embed.py
@@ -1,6 +1,7 @@
 from gensim.models import word2vec
 import numpy as np
 
+
 class UtteranceEmbed():
     
     def __init__(self, file_name, dim=300):
@@ -12,7 +13,20 @@ class UtteranceEmbed():
             print('Error while loading word2vec model')
         
     def embed_utterance(self, utterance):
-        word_embeddings = [self.word2vec_model[word] for word in utterance.split(' ') if word and word in self.word2vec_model]
+        utterance = utterance.lower()
+        word_embeddings = []
+        for word in utterance.split(' '):
+            if len(word):
+                if "'" in word:
+                    pre_word = word.split("'")[0]
+                    after_word = "'" + word.split("'")[1]
+                    if pre_word in self.word2vec_model:
+                        word_embeddings.append(self.word2vec_model[pre_word])
+                    if after_word in self.word2vec_model:
+                        word_embeddings.append(self.word2vec_model[after_word])
+                else:
+                    if word in self.word2vec_model:
+                        word_embeddings.append(self.word2vec_model[word])
         if len(word_embeddings):
             return np.mean(word_embeddings, axis=0)
         else:
